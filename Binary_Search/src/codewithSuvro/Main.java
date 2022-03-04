874700235caccf2a3283cb2aa0dd8c973af3dce7
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-04 23:44:09 +0530

    idea iml file problem solved by deleting the iml file along with the idea file

---
+++ Binary_Search/src/codewithSuvro/Main.java
@@ -0,0 +1,7 @@
+package codewithSuvro;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Binary Search!!");
+    }
+}
--- src/com/codewithSuvro/Main.java
+++
@@ -1,8 +0,0 @@
-package com.codewithSuvro;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
