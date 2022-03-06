f8f1e25e3f40c45fbe0d5da49b7bc833681a8dc2
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-06 09:15:53 +0530

    pre

---
+++ Binary_Search/src/codewithNigga/Developers.java
@@ -0,0 +1,20 @@
+//package codewithNigga;
+//
+//import java.util.Scanner;
+//
+//public class Developers {
+//    public static void main(String[] args) {
+//
+//    }
+//    static void dev()
+//    {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt() ;
+//        int count = 0;
+//            while (n>0)
+//            {
+//
+//            }
+//
+//    }
+//}
---
+++ Binary_Search/src/codewithNigga/Harry.java
@@ -0,0 +1,33 @@
+//package codewithNigga;
+//
+//import java.util.Scanner;
+//
+//public class Harry {
+//    public static void main(String[] args) {
+//        harry();
+//
+//    }
+//    static void harry()
+//    {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt() ;
+//        while (n>0)
+//        {
+//            String str = in.next();
+//            String newstr = "";
+//            for (int i = 0; i <str.length() ;i++)
+//            {
+//                if(i%2==0)
+//                {
+//                    newstr = newstr + str[i];
+//
+//                }
+//            }
+//            System.out.println(newstr);
+//
+//        }
+//        n--;
+//
+//
+//    }
+//}
